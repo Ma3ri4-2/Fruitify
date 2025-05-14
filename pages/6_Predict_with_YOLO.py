9171aa4f583b62f250b852e799383b1fb38232e9
Ma3ri4-2 <mioomalsouri@outlook.com>
2025-05-14 08:48:57 +0300

    Update 6_Predict_with_YOLO.py

--- pages/6_Predict_with_YOLO.py
+++ pages/6_Predict_with_YOLO.py
@@ -6,15 +6,16 @@ st.set_page_config(page_title="🍎 Predict Fruit Quality", layout="centered")
 st.title("🧠 AI Fruit Quality Prediction")
 
 st.markdown("Upload a fruit image to see its predicted condition:")
+st.markdown("Works locally on Jetson's GPUs")
 
-# Load YOLO model
-model = YOLO('././best.pt')
+# # Load YOLO model
+# model = YOLO('././best.pt')
 
-uploaded_file = st.file_uploader("Upload a fruit image", type=["jpg", "jpeg", "png"])
+# uploaded_file = st.file_uploader("Upload a fruit image", type=["jpg", "jpeg", "png"])
 
-if uploaded_file is not None:
-    image = Image.open(uploaded_file)
-    st.image(image, caption='Uploaded Image', use_container_width=True)
+# if uploaded_file is not None:
+#     image = Image.open(uploaded_file)
+#     st.image(image, caption='Uploaded Image', use_container_width=True)
 
-    results = model(image)
-    st.image(results[0].plot(), caption="Prediction", use_container_width=True)
+#     results = model(image)
+#     st.image(results[0].plot(), caption="Prediction", use_container_width=True)
